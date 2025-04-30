f763d79157bd2635bf6e7b33759f7848b7c60527
bella-bhs <abakhshi25@belmontschools.net>
2025-04-30 09:36:27 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const card = document.querySelector('.card__inner');
 
 card.addEvenListener('click', function () {
-  card.classList.toggle('is-flipped')'
+  card.classList.toggle('is-flipped')
 });
