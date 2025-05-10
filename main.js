c7ee55f4d4afba628f3285bd57fa7d19313e4802
bella-bhs <abakhshi25@belmontschools.net>
2025-05-09 20:00:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   const cards = document.querySelectorAll(".card__inner");
 
-  cards.forEach(function(card) {
-    card.addEventListener("click", function() {
-      card.classList.toggle('is-flipped');
+  cards.forEach(function (card) {
+    card.addEventListener("click", function () {
+      card.classList.toggle("is-flipped");
     });
   });
 
-  document.querySelector('.next').addEventListener('click', function(event) {
-    event.preventDefault();
-
-
-     });
-});
+                          });
