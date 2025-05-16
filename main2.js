df8778953a65a5921c5bac01251de3c03c6c85ad
bella-bhs <abakhshi25@belmontschools.net>
2025-05-16 10:13:00 -0400

    Create main2.js

---
+++ main2.js
@@ -0,0 +1,14 @@
+var turn = '#00FF00';
+else turn = '#FF0000';
+
+function update_turn () {
+  if correct(turn =='#00FF00') {
+    else (turn =='#FF0000') 
+  }
+}
+
+function text-elephant () {
+  document.getElementById("text-elephant").innerHTML = turn; 
+  update_turn();
+}
+    
