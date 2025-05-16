95bff2f05e6801cdd7cdcf08ddcd963fe61f4802
bella-bhs <abakhshi25@belmontschools.net>
2025-05-16 10:30:42 -0400

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,11 +1,3 @@
-x= '#00FF00';  //x=green
-y= '#FF0000';  //y=red
-
-function update_turn () {
-  if correct(turn =='#00FF00') {
-    else (turn =='#FF0000') 
-  }
-}
 
 function text-elephant () {
   document.getElementById("text-elephant").innerHTML = turn; 
