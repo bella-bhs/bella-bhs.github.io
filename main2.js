556d16f029504ae5a5683003dd2896bb8514c3eb
bella-bhs <abakhshi25@belmontschools.net>
2025-05-16 12:38:31 -0400

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,1 +1,13 @@
 
+
+function selectText() {
+    selectedText = "text-elephant"; // Store the selected text
+}
+
+    if ("text-elephant" === "img-elephant") {
+       backgroundColor = "green"; 
+    } else {
+       backgroundColor = "red"; 
+    }
+
+   
