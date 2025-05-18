26ab93c827092aa17673004e721c9100987e3e71
bella-bhs <abakhshi25@belmontschools.net>
2025-05-18 10:15:48 -0400

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,7 +1,20 @@
+let firstClick = null; 
 
-function imgElephant() {
-    handleClick(document.getElementById("imgElephant"), "elephant");
+function click (x, y) {
+    if (firstClick===null){
+        firstClick = {x: x, y: y }; 
+    }
+    else {
+        if (firstClick.x === x) {
+            firstClick.y.style.backgroundColor = "lightgreen"; 
+            y.style.backgroundColor = "lightgreen"; 
+        }
+        else { 
+            firstClick.y.style.backgroundColor = "lightred"; 
+            y.style.backgroundColor = "lightred"; 
+        }
+        firstClick = null; 
+    }
 }
-function textElephant() {
-  handleClick(document.getElementById("textElephant"), "elephant");
-}
+
+  
