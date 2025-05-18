737728b53d9f827a3749dbeac0e0b917921aefb9
bella-bhs <abakhshi25@belmontschools.net>
2025-05-18 10:19:09 -0400

    Update main2.js

--- main2.js
+++ main2.js
@@ -6,11 +6,11 @@ function click (x, y) {
     }
     else {
         if (firstClick.x === x) {
-            firstClick.y.style.backgroundColor = "lightgreen"; 
+            firstClick.y.style.backgroundColor = '#8080F0'; 
             y.style.backgroundColor = "lightgreen"; 
         }
         else { 
-            firstClick.y.style.backgroundColor = "lightred"; 
+            firstClick.y.style.backgroundColor = '#F08080'; 
             y.style.backgroundColor = "lightred"; 
         }
         firstClick = null; 
